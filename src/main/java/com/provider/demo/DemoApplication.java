a770b3976e5e9d58cd939eb8da4a8b9b035751dd
liuyang <1269979614@qq.com>
2019-07-07 22:46:05 +0800

    eureka Dalston.SR1 completed

--- src/main/java/com/provider/demo/DemoApplication.java
+++ src/main/java/com/provider/demo/DemoApplication.java
@@ -2,13 +2,15 @@ package com.provider.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {RabbitAutoConfiguration.class})
 @EnableDiscoveryClient
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
 }
